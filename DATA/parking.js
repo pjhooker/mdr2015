b47f0362a977dc281715fb6ab8732f5c140d28c1
LucaMoiana <luca_moiana@hotmail.com>
2015-03-31 00:39:40 +0200

    upload

---
+++ DATA/parking.js
@@ -0,0 +1,66 @@
+var parking = [{
+  "type": "FeatureCollection",
+  "generator": "overpass-turbo",
+  "copyright": "The data included in this document is from www.openstreetmap.org. The data is made available under ODbL.",
+  "timestamp": "2015-01-26T20:35:02Z",
+  "features": [
+    { "type": "Feature",
+    "properties": {
+      "@id": "way\/23225179",
+      "amenity": "parking",
+      "created_by": "JOSM"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+        8.968128103017806,
+        45.657941124691092
+        ]
+        }
+        },
+
+        { "type": "Feature",
+          "properties": {
+            "@id": "way\/23225179",
+            "amenity": "parking",
+            "created_by": "JOSM",
+            },
+            "geometry": {
+              "type": "Point",
+              "coordinates": [
+                8.970021904924128,
+                45.659859035232856
+                ] } },
+
+                { "type": "Feature",
+                  "properties": {
+                    "@id": "way\/23225179",
+                    "amenity": "parking",
+                    "created_by": "JOSM",
+                    },
+                    "geometry": {
+                      "type": "Point",
+                      "coordinates": [
+                        8.971948537818761, 45.657410859837817 ] } },
+                        { "type": "Feature",
+                          "properties": {
+                            "@id": "way\/23225179",
+                            "amenity": "parking",
+                            "created_by": "JOSM",
+                            },
+                            "geometry": {
+                              "type": "Point",
+                              "coordinates": [ 8.974562420627073, 45.658296484211725 ] } },
+                              { "type": "Feature",
+                                "properties": {
+                                  "@id": "way\/23225179",
+                                  "amenity": "parking",
+                                  "created_by": "JOSM",
+                                  },
+                                  "geometry": {
+                                    "type": "Point",
+                                    "coordinates": [ 8.970087543357922, 45.657027608862208   ]
+                                    }
+                                  }
+                                ]
+                              }]
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # mdr2015
-Webmapp della maratonia del Rugareto 2015
+web map of Maratonina del Rugareto 2015
+
+Todo list:
